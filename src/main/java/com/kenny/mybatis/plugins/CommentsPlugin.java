6a7c8ba2eeabb1e90b07b5522a8ec8bb1899f8da
kenny <kehui@imdada.cn>
2017-12-27 15:48:18 +0800

    1

--- src/main/java/com/kenny/mybatis/plugins/CommentsPlugin.java
+++ src/main/java/com/kenny/mybatis/plugins/CommentsPlugin.java
@@ -1,5 +1,6 @@
 package com.kenny.mybatis.plugins;
 
+import org.apache.commons.lang.StringUtils;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
@@ -7,6 +8,8 @@ import org.mybatis.generator.api.dom.java.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.mybatis.generator.config.MergeConstants.NEW_ELEMENT_TAG;
 
 /**
  * Created by kehui on 2015/1/9.
@@ -19,148 +22,32 @@ public class CommentsPlugin extends PluginAdapter {
 
     @Override
     public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        List<String> docLines = initFieldDocLines(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
+        List<String> docLines = initFieldDocLines(introspectedColumn, introspectedTable);
         field.getJavaDocLines().clear();
         field.getJavaDocLines().addAll(docLines);
         return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
     }
 
-    @Override
-    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        List<String> docLines = initSetterMethodDocLines(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
-        method.getJavaDocLines().clear();
-        method.getJavaDocLines().addAll(docLines);
-        return super.modelSetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
-    }
-
-    @Override
-    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        List<String> docLines = initGetterMethodDocLines(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
-        method.getJavaDocLines().clear();
-        method.getJavaDocLines().addAll(docLines);
-        return super.modelGetterMethodGenerated(method, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
-    }
-
-    @Override
-    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        List<String> docLines = initJavaMapperDocLines(interfaze, topLevelClass, introspectedTable);
-        List<Method> methods = interfaze.getMethods();
-        for (Method method : methods) {
-            method.getJavaDocLines().clear();
-            method.getJavaDocLines().addAll(docLines);
-        }
-        return super.clientGenerated(interfaze, topLevelClass, introspectedTable);
-    }
-
-    @Override
-    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        List<String> docLines = initExampleDocLines(topLevelClass, introspectedTable);
-        topLevelClass.getJavaDocLines().clear();
-        topLevelClass.getJavaDocLines().addAll(docLines);
-        List<Field> fields = topLevelClass.getFields();
-        for (Field field : fields) {
-            field.getJavaDocLines().clear();
-            field.getJavaDocLines().addAll(docLines);
-        }
-        List<Method> methods = topLevelClass.getMethods();
-        for (Method method : methods) {
-            method.getJavaDocLines().clear();
-            method.getJavaDocLines().addAll(docLines);
-        }
-        List<InnerClass> innerClasses = topLevelClass.getInnerClasses();
-        for (InnerClass innerClass : innerClasses) {
-            innerClass.getJavaDocLines().clear();
-            innerClass.getJavaDocLines().addAll(docLines);
-        }
-        return super.modelExampleClassGenerated(topLevelClass, introspectedTable);
-    }
-
     /**
      * 字段注释
-     * @param field
-     * @param topLevelClass
      * @param introspectedColumn
      * @param introspectedTable
-     * @param modelClassType
      * @return
      */
-    public List<String> initFieldDocLines(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+    public List<String> initFieldDocLines(IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable) {
         List<String> javaDocLines = new ArrayList<>();
         javaDocLines.add("/**");
-        javaDocLines.add(" * 该字段对应数据库表中的字段" + introspectedTable.getTableConfiguration().getTableName() + "."
+        javaDocLines.add(" * " + introspectedTable.getTableConfiguration().getTableName() + "."
                 + introspectedColumn.getActualColumnName());
-        javaDocLines.add(" * 字段描述：" + introspectedColumn.getRemarks());
-        javaDocLines.add(" * 字段数据库类型：" + introspectedColumn.getJdbcTypeName());
-        javaDocLines.add(" * 字段长度：" + introspectedColumn.getLength());
-        javaDocLines.add(" * @mbggenerated");
+        if (StringUtils.isNotBlank(introspectedColumn.getRemarks())) {
+            javaDocLines.add(" * " + introspectedColumn.getRemarks());
+        }
+        javaDocLines.add(" * " + introspectedColumn.getJdbcTypeName());
+        javaDocLines.add(" * " + introspectedColumn.getLength());
+        javaDocLines.add(" * " + NEW_ELEMENT_TAG);
         javaDocLines.add(" */");
         return javaDocLines;
     }
 
-    /**
-     * 生成setter方法注释
-     * @param method
-     * @param topLevelClass
-     * @param introspectedColumn
-     * @param introspectedTable
-     * @param modelClassType
-     * @return
-     */
-    public List<String> initSetterMethodDocLines(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        List<String> javaDocLines = new ArrayList<>();
-        return javaDocLines;
-    }
 
-    /**
-     * 生成getter方法注释
-     * @param method
-     * @param topLevelClass
-     * @param introspectedColumn
-     * @param introspectedTable
-     * @param modelClassType
-     * @return
-     */
-    public List<String> initGetterMethodDocLines(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        List<String> javaDocLines = new ArrayList<>();
-        return javaDocLines;
-    }
-
-    /**
-     * mapper接口注释
-     * @param topLevelClass
-     * @param introspectedTable
-     * @return
-     */
-    public List<String> initJavaMapperDocLines(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        List<String> javaDocLines = new ArrayList<>();
-        return javaDocLines;
-    }
-
-    /**
-     * example注释
-     * @param topLevelClass
-     * @param introspectedTable
-     * @return
-     */
-    public List<String> initExampleDocLines(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        List<String> javaDocLines = new ArrayList<>();
-        return javaDocLines;
-    }
-
-    /**
-     * xml注释
-     * @param field
-     * @param topLevelClass
-     * @param introspectedColumn
-     * @param introspectedTable
-     * @param modelClassType
-     * @return
-     */
-    public List<String> initSqlMapperDocLines(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
-        List<String> xmlDocLines = new ArrayList<>();
-        xmlDocLines.add("<!--");
-        xmlDocLines.add("  WARNING - AutoGenerate file, don't update any code!");
-        xmlDocLines.add("-->");
-        return xmlDocLines;
-    }
 }
